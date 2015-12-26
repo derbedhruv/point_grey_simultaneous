09fcbdf8f5b73cdfd94a13329b8e2258e0d770e4
Dhruv <dhruv.joshi.1989@gmail.com>
2015-12-26 17:31:51 +0530

    made the slit image size double but no difference found in the final thing. Synchronization and green slit happening but it is very slow, needs to be sped up

--- MultipleCameraEx.cpp
+++ MultipleCameraEx.cpp
@@ -104,7 +104,7 @@ int main(int argc, char* argv[]) {
 
 	// no of images to capture is given as a numerical argument to the program
 	// check if this has actuallybeen given, or else use default..
-	unsigned int numImages = 5;
+	unsigned int numImages = 100;
 	if (argc < 2) {
 	  printf("No parameter entered for number of images, going with default 50.\n");
 	} else {
@@ -115,7 +115,7 @@ int main(int argc, char* argv[]) {
         vecImages1.resize(numImages);
 	std::vector<Image> vecImages2;
         vecImages2.resize(numImages);
-	int slitRow = 600, slitCol = 800, slitStart = 250, slitMove = 1;
+	int slitRow = 1200, slitCol = 1600, slitStart = 500, slitMove = 1;
 	cv::Mat projectedSlit(slitRow, slitCol, CV_8UC1);
 	cv::cvtColor(projectedSlit, projectedSlit, CV_GRAY2RGB);
 	cout << projectedSlit.at<cv::Vec3b>(0,0).val[0] << endl;
@@ -130,9 +130,6 @@ int main(int argc, char* argv[]) {
 	black.val[0] = 0; black.val[1] = 0; black.val[2] = 0;
 	green.val[0] = 0; green.val[1] = 255; green.val[2] = 0;
 
-
-	cout << "before the loop" <<endl;
-
 	for (unsigned int j=0; j < numImages; j++ ) {
 	    // first display the window with the slit
 	    // We will update the Mat object and update the slit position
@@ -142,13 +139,12 @@ int main(int argc, char* argv[]) {
 		     projectedSlit.at<cv::Vec3b>(cv::Point(slitStart + (j - 1)*slitMove, a)) = black;
 		    // projectedSlit.at<uchar>(a, slitStart + (j - 1)*slitMove) = 0;
 		}
-		cout << a << ", " << j << endl;
 		projectedSlit.at<cv::Vec3b>(cv::Point(slitStart + j*slitMove, a)) = green;
 		// projectedSlit.at<uchar>(a, slitStart + j*slitMove) = 255;
 	    }
 
 	    cv::imshow("Image1", projectedSlit);
-	    cv::waitKey(10);
+	    cv::waitKey(1);
 
 
 	    // then we capture the image from both cameras
